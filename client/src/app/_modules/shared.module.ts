ebcdf0af103fa7e3015e836421b698b92a0178b4
blastsai <vuthanhvan0123456@gmail.com>
2023-07-11 09:55:20 +0700

    End of section 10

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
-
 @NgModule({
   declarations: [],
   imports: [
---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,59 @@
+import { Component, Host, HostListener, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs';
+import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm | undefined;
+  member!: Member;
+  user!: User;
+  activeTab: string = 'tab1'
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
+    }
+  }
+
+  constructor(private accountService: AccountService, private memberService: MembersService, private toastr: ToastrService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user!);
+  }
+
+  ngOnInit() {
+    this.loadMember();
+  }
+
+  loadMember() {
+    this.memberService.getMember(this.user.username).subscribe(member => {
+      this.member = member;
+    })
+  }
+
+  updateMember() {
+    this.memberService.updateMember(this.member).subscribe(() => {
+      this.toastr.success('Profile updated successfully');
+      this.editForm?.reset(this.member);
+    })
+
+  }
+
+  openTab(tabName: string): void {
+    this.activeTab = tabName;
+    const tabContents = document.getElementsByClassName("tab-content");
+    for (let i = 0; i < tabContents.length; i++) {
+      (tabContents[i] as HTMLElement).style.display = "none";
+    }
+    const selectedTab = document.getElementById(tabName);
+    if (selectedTab) {
+      (selectedTab as HTMLElement).style.display = "block";
+    }
+  }
+}
