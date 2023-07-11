597a0ff49a8bcdbd17772b781f34894c239cabcb
blastsai <vuthanhvan0123456@gmail.com>
2023-07-11 16:37:59 +0700

    End of section 11

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -1,8 +1,8 @@
 export interface User {
     username: string;
     token: string;
+    photoUrl: string;
 }
 
-const multiply = (x: number,y: number) =>  x*y;
 
 
--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,19 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { FileUploadModule } from 'ng2-file-upload';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    BsDropdownModule,
     ToastrModule.forRoot({
       positionClass: "toast-bottom-right"
     }),
-    NgxGalleryModule
+    NgxGalleryModule,
+    FileUploadModule
   ],
   exports: [
     ToastrModule,
-    NgxGalleryModule
+    NgxGalleryModule,
+    FileUploadModule
   ]
 })
 export class SharedModule { }
